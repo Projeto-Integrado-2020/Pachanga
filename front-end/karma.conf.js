5e8bd682f726d89af20ccfd14149b06c79ea7056
AndreyLacerda <andrey-lacerda@hotmail.com>
2020-04-26 13:32:38 -0300

    Corrigindo navegador de teste karma para o travis

--- front-end/karma.conf.js
+++ front-end/karma.conf.js
@@ -3,6 +3,13 @@
 
 module.exports = function (config) {
   config.set({
+    browsers: ['MyHeadlessChrome'],
+    customLaunchers: {
+      MyHeadlessChrome: {
+        base: 'ChromeHeadless',
+        flags: ['--disable-translate', '--disable-extensions', '--remote-debugging-port=9223', '--no-sandbox' ]
+      }
+    },
     basePath: '',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
@@ -25,17 +32,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['MyHeadlessChrome'],
-    customLaunchers: {
-      MyHeadlessChrome: {
-        base: 'ChromeHeadless',
-        flags: ['--disable-translate', '--disable-extensions', '--remote-debugging-port=9223', '--no-sandbox' ]
-      }
-    },
     singleRun: false,
     restartOnFileChange: true
   });
-  if(process.env.TRAVIS){
-    config.browsers = ['Chrome_travis_ci'];
-  }
 };
