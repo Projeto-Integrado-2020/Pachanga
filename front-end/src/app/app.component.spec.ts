7644d7fa4fec3d26a6c6cf47dd567fbee7e89d11
tiagoMurer <tiagostaurenghi@hotmail.com>
2020-04-26 18:35:40 -0300

    correcao de falha em teste 'should render title' no app.component.spec.ts que estava configurado com base na pagina default

--- front-end/src/app/app.component.spec.ts
+++ front-end/src/app/app.component.spec.ts
@@ -26,6 +26,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('front-end app is running!');
+    expect(compiled.querySelector('.hello-world h1').textContent).toContain('Hello world');
   });
 });
