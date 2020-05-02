06b52b4b58b0d3845105f65a536e2c4c6a715baf
Luis Antonio <luis.antonio@aluno.ifsp.edu.br>
2020-05-02 17:32:02 -0300

    Página 'index' feita (sem 'href').

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -2,15 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {
+  MatToolbarModule, 
+  MatIconModule, 
+  MatButtonModule,
+  MatGridListModule
+} from '@angular/material';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { };
