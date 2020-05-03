7a8122d557b90a61b63f57b5e3a51ef7db850d3b
guga72 <guga.72@hotmail.com>
2020-05-02 21:36:39 -0300

    Modificações na configuração do banco de dados, do Heroku e do Local

---
+++ back-end/src/main/java/com/eventmanager/pachanga/utils/DataBaseConfigLocal.java
@@ -0,0 +1,38 @@
+package com.eventmanager.pachanga.utils;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+@Configuration
+@Profile("dev")
+public class DataBaseConfigLocal {
+	@Bean
+    public DataSource dataSource(){
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.postgresql.Driver");
+//        dataSource.setUrl("jdbc:postgresql://localhost:5432/pachanga");
+        dataSource.setUrl("jdbc:postgresql://localhost:5433/pachanga");
+        dataSource.setUsername("postgres");
+//        dataSource.setPassword("1234"); // sua senha do postgres local
+        dataSource.setPassword("g1u9g4a3");
+        return dataSource;
+    }
+
+    @Bean
+    public JpaVendorAdapter jpaVendorAdapter(){
+        HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+        adapter.setDatabase(Database.POSTGRESQL);
+        adapter.setShowSql(true);
+        adapter.setGenerateDdl(true);
+        adapter.setDatabasePlatform("org.hibernate.dialect.PostgreSQLDialect");
+        adapter.setPrepareConnection(true);
+        return adapter;
+    }
+}
