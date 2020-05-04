dcfbc1e5d7ed31efa2e937785ccfd9ba850743bb
guga72 <guga.72@hotmail.com>
2020-05-04 17:49:38 -0300

    Usuario pegando o nextval da sequencia e modificacao da comunicação do controller com o service

--- back-end/src/main/java/com/eventmanager/pachanga/controllers/UsuarioController.java
+++ back-end/src/main/java/com/eventmanager/pachanga/controllers/UsuarioController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.eventmanager.pachanga.domains.Usuario;
 import com.eventmanager.pachanga.dtos.UsuarioTO;
-import com.eventmanager.pachanga.repositories.UsuarioRepository;
 import com.eventmanager.pachanga.services.UsuarioService;
 
 @Controller
@@ -18,13 +17,11 @@ import com.eventmanager.pachanga.services.UsuarioService;
 public class UsuarioController{
 	
 	@Autowired
-	private UsuarioRepository userRepository;
-	
-	private final UsuarioService userService = new UsuarioService();
+	private UsuarioService userService;
 	
 	@RequestMapping(path ="/cadastro", method = RequestMethod.POST)
 	public ResponseEntity<UsuarioTO> cadastro(@RequestBody Usuario user) {
-		Boolean usuarioJaCadastrado = userService.cadastro(user, userRepository);
+		Boolean usuarioJaCadastrado = userService.cadastro(user);
 		if(usuarioJaCadastrado) {
 			UsuarioTO userto = new UsuarioTO(user.getNomeUser(), user.getTipConta(), user.getDtNasc(), user.getSexo());
 			return new ResponseEntity<UsuarioTO>(userto, HttpStatus.OK);
@@ -34,7 +31,7 @@ public class UsuarioController{
 	
 	@RequestMapping(path ="/login", method = RequestMethod.POST)
 	public ResponseEntity<UsuarioTO> login(@RequestBody Usuario user) {
-		Usuario usarioLogin = userService.login(user, userRepository);
+		Usuario usarioLogin = userService.login(user);
 		if(usarioLogin != null) {
 			UsuarioTO userto = new UsuarioTO(usarioLogin.getNomeUser(), usarioLogin.getTipConta(), usarioLogin.getDtNasc(), usarioLogin.getSexo());
 			return new ResponseEntity<UsuarioTO>(userto, HttpStatus.OK);
--- back-end/src/main/java/com/eventmanager/pachanga/repositories/UsuarioRepository.java
+++ back-end/src/main/java/com/eventmanager/pachanga/repositories/UsuarioRepository.java
@@ -1,10 +1,16 @@
 package com.eventmanager.pachanga.repositories;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.eventmanager.pachanga.domains.Usuario;
 
+@Repository
 public interface UsuarioRepository extends CrudRepository<Usuario, Integer>{
+	
+	@Query(value = "SELECT NEXTVAL('seq_usuario');", nativeQuery = true)
+	public int getNextValMySequence();
 	
 	Usuario findByEmail(String email);
 	
