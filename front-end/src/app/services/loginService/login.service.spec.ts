ecc92547dc90df9dee5d7aea7e7b04962b0ef859
gghosth <42755106+gghosth@users.noreply.github.com>
2020-05-05 17:52:11 -0300

    Adicionando services da maneira certa

--- front-end/src/app/services/loginService/login.service.spec.ts
+++ front-end/src/app/services/loginService/login.service.spec.ts
@@ -3,10 +3,14 @@ import { TestBed } from '@angular/core/testing';
 import { LoginService } from './login.service';
 
 describe('LoginService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let service: LoginService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(LoginService);
+  });
 
   it('should be created', () => {
-    const service: LoginService = TestBed.get(LoginService);
     expect(service).toBeTruthy();
   });
 });
