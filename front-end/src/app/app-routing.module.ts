c12f679bfd2fe4b6b74453ad56ce78d7a41fe9f2
gghosth <42755106+gghosth@users.noreply.github.com>
2020-05-07 15:50:29 -0300

    aplicando servico authguard

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CanActivate } from '@angular/router';
 import { LoginComponent } from './views/login/login.component';
 import { CadastroComponent } from './views/cadastro/cadastro.component';
 import { IndexComponent } from './views/index/index.component';
