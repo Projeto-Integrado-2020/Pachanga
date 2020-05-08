1ed3c65769a73d57478bc39ca5855ba94ef8125b
gghosth <42755106+gghosth@users.noreply.github.com>
2020-05-08 18:20:53 -0300

    tirando imports nao utilizados

--- back-end/src/main/java/com/eventmanager/pachanga/controllers/UsuarioController.java
+++ back-end/src/main/java/com/eventmanager/pachanga/controllers/UsuarioController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+
 
 import com.eventmanager.pachanga.domains.Usuario;
 import com.eventmanager.pachanga.dtos.UsuarioTO;
