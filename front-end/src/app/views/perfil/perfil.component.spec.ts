9a543d158ea8ea1dd6b5d3dbb0fca94bc513e4de
tiagoMurer <tiagostaurenghi@hotmail.com>
2020-05-21 19:40:18 -0300

    trabalho na pagina de perfil do usuario

--- front-end/src/app/views/perfil/perfil.component.spec.ts
+++ front-end/src/app/views/perfil/perfil.component.spec.ts
@@ -3,6 +3,10 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { CustomMaterialModule } from '../material/material.module';
 
 import { PerfilComponent } from './perfil.component';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { HttpLoaderFactory } from '../cadastro/cadastro.component.spec';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('PerfilComponent', () => {
   let component: PerfilComponent;
@@ -12,7 +16,18 @@ describe('PerfilComponent', () => {
     jasmine.DEFAULT_TIMEOUT_INTERVAL = 100000;
     TestBed.configureTestingModule({
       declarations: [ PerfilComponent ],
-      imports: [ CustomMaterialModule ]
+      imports: [
+        BrowserAnimationsModule,
+        CustomMaterialModule,
+        HttpClientModule,
+        TranslateModule.forRoot({
+          loader: {
+            provide: TranslateLoader,
+            useFactory: HttpLoaderFactory,
+            deps: [HttpClient]
+          }
+        }),
+      ]
     })
     .compileComponents();
   }));
--- front-end/src/app/views/perfil/perfil.component.ts
+++ front-end/src/app/views/perfil/perfil.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { FormControl, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-perfil',
@@ -7,7 +9,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PerfilComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
