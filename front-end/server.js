a523e23f03aad07043ec15b11681c13701e6c549
AndreyLacerda <andrey-lacerda@hotmail.com>
2020-07-10 15:21:55 -0300

    Adicionando SSL Heroku redirect

--- front-end/server.js
+++ front-end/server.js
@@ -1,11 +1,14 @@
 //Install express server
 const express = require('express');
 const path = require('path');
+const sslRedirect = require('heroku-ssl-redirect');
 
 const app = express();
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/front-end'));
+
+app.use(sslRedirect());
 
 app.get('/*', function(req,res) {
     
