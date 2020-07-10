ac64e33f20d68eb65241a79e7c052eebdee791ae
AndreyLacerda <andrey-lacerda@hotmail.com>
2020-07-10 18:42:48 -0300

    Adicionando rota para certificacao SSL

--- front-end/server.js
+++ front-end/server.js
@@ -11,8 +11,11 @@ app.use(express.static(__dirname + '/dist/front-end'));
 app.use(sslRedirect());
 
 app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/front-end/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/front-end/index.html'));
+});
+
+app.get('/.well-known/acme-challenge/3hd9Vr3ja-PduaO0FCObvmbyUH4zaS3DFdUEhNU2LT4', function(req, res) {
+    res.send('3hd9Vr3ja-PduaO0FCObvmbyUH4zaS3DFdUEhNU2LT4.z9Coz9YXNvl8QrQOr6k2A2tWbudvS5xiXHjd-tHQPIc')
 });
 
 // Start the app by listening on the default Heroku port
