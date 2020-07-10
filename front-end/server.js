ce0aeacfc160e3fd6141106856098021c26cfa8a
AndreyLacerda <andrey-lacerda@hotmail.com>
2020-07-10 19:36:33 -0300

    Subindo redirect http-https via angular

--- front-end/server.js
+++ front-end/server.js
@@ -1,17 +1,15 @@
 //Install express server
 const express = require('express');
 const path = require('path');
-const sslRedirect = require('heroku-ssl-redirect');
 
 const app = express();
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/front-end'));
 
-app.use(sslRedirect());
 
-app.get('/.well-known/acme-challenge/3tjbjSzDmDxYfSKeLkZ9aeJ0pdJaIdyE6wXgEu-mmt8', function(req, res) {
-    res.send('3tjbjSzDmDxYfSKeLkZ9aeJ0pdJaIdyE6wXgEu-mmt8.z9Coz9YXNvl8QrQOr6k2A2tWbudvS5xiXHjd-tHQPIc');
+app.get('/*', function(req,res) { 
+    res.sendFile(path.join(__dirname+'/dist/front-end/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
