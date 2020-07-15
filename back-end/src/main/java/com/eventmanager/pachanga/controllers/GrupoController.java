92550da470aeeb4e80fe8c71fdd0af2f88146fe4
guga72 <guga.72@hotmail.com>
2020-07-15 20:48:38 -0300

    Criação do serviço para envio de email para usuários

--- back-end/src/main/java/com/eventmanager/pachanga/controllers/GrupoController.java
+++ back-end/src/main/java/com/eventmanager/pachanga/controllers/GrupoController.java
@@ -1,19 +1,37 @@
 package com.eventmanager.pachanga.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.eventmanager.pachanga.errors.ValidacaoException;
+import com.eventmanager.pachanga.services.GrupoService;
 
 @Controller
 @RequestMapping("/grupo")
 @CrossOrigin
 public class GrupoController {
 	
-	@PostMapping("/addUserGrupo")
-	public ResponseEntity<Object> adicionarUsuarioGrupo(){
-		return null;
+	@Autowired
+	private GrupoService grupoService;
+	
+	@ResponseBody
+	@GetMapping(path = "/addUserFesta")
+	public ResponseEntity<Object> addUsuarioFesta(@RequestParam(required = true)int codFesta, @RequestBody List<String> emails){
+		try {
+			StringBuilder mensagem = grupoService.addUsuariosFesta(emails, codFesta);
+			return ResponseEntity.ok(mensagem);
+		} catch (ValidacaoException e) {
+			return ResponseEntity.status(400).body(e.getMessage());
+		}
 	}
 
 }
