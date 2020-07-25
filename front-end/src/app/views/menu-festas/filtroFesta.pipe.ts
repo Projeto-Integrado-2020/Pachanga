4edb4911594549389b6168a4175a5bab1fef3001
tiagoMurer <tiagostaurenghi@hotmail.com>
2020-07-25 16:29:44 -0300

    correcoes lint

--- front-end/src/app/views/menu-festas/filtroFesta.pipe.ts
+++ front-end/src/app/views/menu-festas/filtroFesta.pipe.ts
@@ -1,9 +1,9 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({name: 'filtroFesta'})
-export class FiltroFestaPipe implements PipeTransform{
+export class FiltroFestaPipe implements PipeTransform {
     transform(festasMostradas: any, buscaPorNome: string): any {
-        if (!festasMostradas || !buscaPorNome){
+        if (!festasMostradas || !buscaPorNome) {
             return festasMostradas;
         }
 
