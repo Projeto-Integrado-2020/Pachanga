dfa6549a75d9e30757cbfe35fe12cd496a485d51
gghosth <42755106+gghosth@users.noreply.github.com>
2020-07-26 13:27:33 -0300

    adicionando services basicos

--- front-end/src/app/views/gerenciador-membros/gerenciador-membros.component.ts
+++ front-end/src/app/views/gerenciador-membros/gerenciador-membros.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-gerenciador-membros',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GerenciadorMembrosComponent implements OnInit {
 
-  constructor() { }
+  constructor(public formBuilder: FormBuilder, public getFestaService: GetFestaService) { }
 
   ngOnInit(): void {
   }
