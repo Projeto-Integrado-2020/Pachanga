cf6d5555eeeb845a927a6324eba76ac7c686142d
gghosth <42755106+gghosth@users.noreply.github.com>
2020-07-26 13:28:02 -0300

    consertando erro ao esquecer de salvar

--- front-end/src/app/views/gerenciador-membros/gerenciador-membros.component.ts
+++ front-end/src/app/views/gerenciador-membros/gerenciador-membros.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
+import { GetFestaService } from 'src/app/services/get-festa/get-festa.service';
 
 @Component({
   selector: 'app-gerenciador-membros',
