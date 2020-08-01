6ed4dba297166c88708956aaa43283d87ec25e7e
AndreyLacerda <andrey-lacerda@hotmail.com>
2020-08-01 13:07:19 -0300

    Subindo correcao de testes e back-end para prod

--- front-end/src/app/views/distribuicao-permissoes/distribuicao-permissoes.component.spec.ts
+++ front-end/src/app/views/distribuicao-permissoes/distribuicao-permissoes.component.spec.ts
@@ -1,6 +1,9 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DistribuicaoPermissoesComponent } from './distribuicao-permissoes.component';
+import { RouterModule } from '@angular/router';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { CustomMaterialModule } from '../material/material.module';
 
 describe('DistribuicaoPermissoesComponent', () => {
   let component: DistribuicaoPermissoesComponent;
@@ -8,7 +11,12 @@ describe('DistribuicaoPermissoesComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ DistribuicaoPermissoesComponent ]
+      declarations: [ DistribuicaoPermissoesComponent ],
+      imports: [
+        RouterModule.forRoot([]),
+        HttpClientTestingModule,
+        CustomMaterialModule
+      ]
     })
     .compileComponents();
   }));
