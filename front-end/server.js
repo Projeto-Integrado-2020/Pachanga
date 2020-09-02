c83664d170bb1dd59231506074b5a36d370be0af
AndreyLacerda <andrey-lacerda@hotmail.com>
2020-09-02 12:30:51 -0300

    Adicionando headers de seguranca e compress de build

--- front-end/server.js
+++ front-end/server.js
@@ -2,8 +2,37 @@
 const express = require('express');
 const path = require('path');
 const compression = require('compression');
-
+const sts = require('strict-transport-security');
+const csp = require('content-security-policy');
+const helmet = require('helmet');
+const featurePolicy = require('feature-policy');
+const referrerPolicy = require('referrer-policy');
 const app = express();
+
+const cspPolicy = {
+  'report-uri': '/reporting',
+  'default-src': csp.SRC_NONE,
+  'script-src': [ csp.SRC_SELF, csp.SRC_DATA ]
+};
+
+
+const globalCSP = csp.getCSP(csp.STARTER_OPTIONS);
+
+const globalSTS = sts.getSTS({'max-age': 31536000, 'includeSubDomains': true});
+ 
+// This will apply this policy to all requests
+app.use(referrerPolicy({ policy: 'same-origin' }));
+app.use(globalSTS);
+app.use(globalCSP);
+app.use(helmet.frameguard())
+app.use(featurePolicy({
+  features: {
+    fullscreen: ["'self'"],
+    vibrate: ["'none'"],
+    payment: ['example.com'],
+    syncXhr: ["'none'"]
+  }
+}));
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/front-end'));
