47bd4144addd8d8b5a48590dea0514822328500a
tiagoMurer <tiagostaurenghi@hotmail.com>
2020-11-21 18:58:45 -0300

    mockzinho na pag de meus ingressos

---
+++ front-end/src/app/services/ingressos/ingressos.service.ts
@@ -0,0 +1,54 @@
+import { HttpClient, HttpErrorResponse, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { throwError } from 'rxjs';
+import { catchError, take } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
+import { LogService } from '../logging/log.service';
+import { LoginService } from '../loginService/login.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class IngressosService {
+
+  private readonly baseUrl = `${environment.URL_BACK}ingresso`;
+  public farol = false;
+
+  constructor(
+    private httpClient: HttpClient,
+    private loginService: LoginService,
+    private logService: LogService,
+    private router: Router
+    ) { }
+
+  metodo1() {
+    if (!this.farol) {
+    const httpParams = new HttpParams()
+      .append('idUser', this.loginService.getusuarioInfo().codUsuario);
+
+    let headers = new HttpHeaders();
+    headers = headers.append('Content-Type', 'application/json');
+    headers = headers.append('Authorization', 'Bearer ' + JSON.parse(localStorage.getItem('token')).token);
+
+    return this.httpClient.get(this.baseUrl + '/listaUser', {params: httpParams, headers}).pipe(
+      take(1),
+      catchError(error => {
+        return this.handleError(error, this.logService);
+      })
+    );
+    }
+  }
+
+  handleError = (error: HttpErrorResponse, logService: LogService) => {
+    logService.initialize();
+    logService.logHttpInfo(JSON.stringify(error), 0, error.url);
+    this.setFarol(false);
+    //this.router.navigate(['404']);
+    return throwError(error);
+  }
+
+  setFarol(flag: boolean) {
+    this.farol = flag;
+  }
+}
--- front-end/src/app/views/meus-ingressos/meus-ingressos.component.ts
+++ front-end/src/app/views/meus-ingressos/meus-ingressos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { IngressosService } from 'src/app/services/ingressos/ingressos.service';
 
 @Component({
   selector: 'app-meus-ingressos',
@@ -7,9 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MeusIngressosComponent implements OnInit {
 
-  constructor() { }
+  infoIngresso: any = [
+    {
+    endereco: 'Rua das Caneleiras, 27',
+    data: '21:00, 28 de Janeiro de 2019'
+    },
+    {
+      endereco: 'Rua Asasd, 237',
+      data: '8:00, 16 de Janeiro de 2019'
+    }
+  ];
+  constructor(private ingressosService: IngressosService) { }
 
   ngOnInit() {
+    this.printarMetodo1();
+  }
+
+  printarMetodo1() {
+    this.ingressosService.metodo1().subscribe(
+      (res) => {
+        console.log(res);
+      }
+    );
   }
 
 }
