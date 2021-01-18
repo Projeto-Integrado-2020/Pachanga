41fb3c53c89d5eb0a3639fee8ea533459dfe5eb8
Pedrofreitas18 <opedrofreitas@gmail.com>
2021-01-18 21:06:01 -0200

    corrigir testes quebrados no travis

--- back-end/src/test/java/com/eventmanager/pachanga/utils/EmailMensagemTest.java
+++ back-end/src/test/java/com/eventmanager/pachanga/utils/EmailMensagemTest.java
@@ -92,11 +92,11 @@ public class EmailMensagemTest {
 	
 	@Test
 	public void enviarEmailQRCodeTest() throws Exception {
-		EmailMensagem.enviarEmailQRCode("teste@email.invalid", festaTest(), listaIngressoTest());
+		EmailMensagem.enviarEmailQRCode("eventmanager72@gmail.com", festaTest(), listaIngressoTest());
 	}
 	
 	@Test
 	public void enviarEmailTest() throws Exception {
-		EmailMensagem.enviarEmail("teste@email.invalid", "haha", festaTest());
+		EmailMensagem.enviarEmail("eventmanager72@gmail.com", "haha", festaTest());
 	}
 }
